fee55cc00f549c8ba4ef083531a671e800f72263
George <georgelu333@hotmail.com>
2021-11-14 13:11:57 +1300

    reformatted some code

--- graphql/index.js
+++ graphql/index.js
@@ -1,37 +1,42 @@
 // Import Apollo Azure integration library
 
-const config = require('./utils/config');
+const mongoose = require('mongoose');
+const axios = require('axios');
 
-const mongoose = require('mongoose')
-const Listing = require('./models/listing')
-
-// const MONGODB_URI = ''
-
-const axios =require('axios');
-
-// https://eva.pingutil.com/
+// connect to https://eva.pingutil.com/ for email validation
 
 const { ApolloServer, gql, UserInputError } = require('apollo-server-azure-functions');
+const Listing = require('./models/listing');
+const config = require('./utils/config');
 
 const validateEmailService = async (emailToValidate) => {
-    const queryUrl = 'https://api.eva.pingutil.com/email?email=' + emailToValidate//test@mail7.io'
-    const response = await axios.get(queryUrl)
-    console.log("validate email")
-    console.log(emailToValidate)
-    console.log(response.data.data.valid_syntax);
-    console.log('----------------------------')
-    return response.data.data.valid_syntax//rate;
+  const queryUrl = `https://api.eva.pingutil.com/email?email=${emailToValidate}`;
+  const response = await axios.get(queryUrl);
+  // eslint-disable-next-line no-console
+  console.log('validate email');
+  // eslint-disable-next-line no-console
+  console.log(emailToValidate);
+  // eslint-disable-next-line no-console
+  console.log(response.data.data.valid_syntax);
+  // eslint-disable-next-line no-console
+  console.log('----------------------------');
+  return response.data.data.valid_syntax;
 };
 
-console.log('connecting to', config.MONGODB_URI)
+// eslint-disable-next-line no-console
+console.log('connecting to', config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true}) //, useFindAndModify: false, useCreateIndex: true })
+// connect to mopngo db
+// unused parameters: , useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    // eslint-disable-next-line no-console
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    // eslint-disable-next-line no-console
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -100,40 +105,39 @@ const resolvers = {
   Query: {
     listingCount: () => Listing.collection.countDocuments(),
     allListings: (root, args) => {
-      console.log(args)
+      // eslint-disable-next-line no-console
+      console.log(args);
       if (!args.phone) {
-        return Listing.find({})
+        return Listing.find({});
       }
-      return Listing.find({ phone: { $exists: args.phone === 'YES'  }})
+      return Listing.find({ phone: { $exists: args.phone === 'YES' } });
     },
     findListing: (root, args) => Listing.findOne({ name: args.name }),
-    findListingById: (root, args) => Listing.findById({ id: args.id })
+    findListingById: (root, args) => Listing.findById({ id: args.id }),
   },
   Listing: {
-    address: (root) => {
-      return {
-        street: root.street,
-        city: root.city
-      }
-    }
+    address: (root) => ({
+      street: root.street,
+      city: root.city,
+    }),
   },
   Mutation: {
     addListing: async (root, args) => {
-      const listing = new Listing({ ...args })
+      const listing = new Listing({ ...args });
       await validateEmailService(listing.emailAddress).then(
         (valid) => {
           // console.log("email validated")
           // console.log(valid)
           // if (valid) {
-            // console.log('valid')
+          // console.log('valid')
           // }
-          if (!valid){
-           // console.log(" not valid")
-            throw new UserInputError("invalid email", {
+          if (!valid) {
+            // console.log(" not valid")
+            throw new UserInputError('invalid email', {
               invalidArgs: args,
-            })
+            });
           }
-        }
+        },
       );
       try {
         // console.log('saving')
@@ -152,76 +156,76 @@ const resolvers = {
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
+        });
       }
     },
     editNumber: async (root, args) => {
-      const listing = await Listing.findOne({ name: args.name })
-      listing.phone = args.phone
+      const listing = await Listing.findOne({ name: args.name });
+      listing.phone = args.phone;
       try {
-        await listing.save()
+        await listing.save();
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
+        });
       }
     },
     editEmailAddress: async (root, args) => {
-      const listing = await Listing.findOne({ name: args.name })
-      listing.emailAddress = args.emailAddress
+      const listing = await Listing.findOne({ name: args.name });
+      listing.emailAddress = args.emailAddress;
       await validateEmailService(args.emailAddress).then(
         (valid) => {
-          //if (valid) {
-            //console.log('valid')
-          //}
-          if (!valid){
-           // console.log(" not valid")
-            throw new UserInputError("invalid email", {
+          // if (valid) {
+          // console.log('valid')
+          // }
+          if (!valid) {
+            // console.log(" not valid")
+            throw new UserInputError('invalid email', {
               invalidArgs: args,
-            })
+            });
           }
-        }
+        },
       );
       try {
-        await listing.save()
+        await listing.save();
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
+        });
       }
     },
 
     editListing: async (root, args) => {
-      const listing = await Listing.findOne({ name: args.name })
-      listing.phone = args.phone
-      listing.street = args.street
-      listing.city = args.city
-      listing.emailAddress = args.emailAddress
-      listing.category = args.category
-      listing.description = args.description
+      const listing = await Listing.findOne({ name: args.name });
+      listing.phone = args.phone;
+      listing.street = args.street;
+      listing.city = args.city;
+      listing.emailAddress = args.emailAddress;
+      listing.category = args.category;
+      listing.description = args.description;
 
       await validateEmailService(args.emailAddress).then(
         (valid) => {
           // if (valid) {
-            // console.log('valid')
+          // console.log('valid')
           // }
-          if (!valid){
-           // console.log(" not valid")
-            throw new UserInputError("invalid email", {
+          if (!valid) {
+            // console.log(" not valid")
+            throw new UserInputError('invalid email', {
               invalidArgs: args,
-            })
+            });
           }
-        }
+        },
       );
       try {
-        await listing.save()
+        await listing.save();
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
+        });
       }
-    }
-  }
+    },
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
--- graphql/models/listing.js
+++ graphql/models/listing.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     unique: true,
-    minlength: 1
+    minlength: 1,
   },
   phone: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
   street: {
     type: String,
     required: true,
-    minlength: 1
+    minlength: 1,
   },
   city: {
     type: String,
     required: true,
-    minlength: 1
+    minlength: 1,
   },
   emailAddress: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
   category: {
     type: String,
     required: true,
-    minlength: 1
+    minlength: 1,
   },
   description: {
     type: String,
     required: true,
-    minlength: 1
+    minlength: 1,
   },
-})
+});
 
-module.exports = mongoose.model('Listing', schema)
+module.exports = mongoose.model('Listing', schema);
